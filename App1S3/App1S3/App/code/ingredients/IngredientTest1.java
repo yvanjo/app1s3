c4ffa1d99785449028fd18f7d54140d7f0714773
Jocelyn yvan <nguj0801@usherbrooke.ca>
2020-09-07 20:19:46 -0400

    test ingredient check out

--- App1S3/App1S3/App/code/ingredients/IngredientTest1.java
+++ App1S3/App1S3/App/code/ingredients/IngredientTest1.java
@@ -8,31 +8,64 @@ import static org.junit.Assert.*;
 
 public class IngredientTest1 {
 
-    /*
+
     @org.junit.Test
-    */
+
 
     /*
         Test de validation des quantitées
         il faut s'assurer que toute les méthodes peuvent acceder à leur quantité
      */
-    /*
+
     public void test1() throws IngredientException {
         Boolean succeed = true;
-        Ingredient fruit = Fruit.getInstanceFruit("liquid");
-        Ingredient viande = Viande.getInstanceViande("liquid");
-        Ingredient legume =  Legume.getInstanceLegume("liquid");
-        Ingredient laitier =  Laitier.getInstanceLaitier("liquid");
-        Ingredient epice =  Epice.getInstanceEpice("liquid");
-        IngredientInventaire ingredientInventaireFruit = new IngredientInventaire(fruit ,134.00);
-        IngredientInventaire ingredientInventaireViande = new IngredientInventaire(viande ,132344.60);
-        IngredientInventaire ingredientInventaireLegume = new IngredientInventaire(legume ,123434.60);
-        IngredientInventaire ingredientInventaireLaitier = new IngredientInventaire(laitier ,13234.60);
-        IngredientInventaire ingredientInventaireEpice = new IngredientInventaire(epice ,234552344546.245);
+        ConcretCreatorFruit fruit = new ConcretCreatorFruit();
+        Ingredient fruit1 = fruit.CreateMethod("fruit 1", new StateSolide());
+        ConcretCreatorViande viande = new ConcretCreatorViande();
+        Ingredient viande1 = viande.CreateMethod("viande 1", new StateSolide());
+
+        ConcretCreatorLegume legume = new ConcretCreatorLegume();
+        Ingredient legume1 = legume.CreateMethod("legume 1", new StateSolide());
+        ConcretCreatorLaitier laitier = new ConcretCreatorLaitier();
+        Ingredient laitier1 = laitier.CreateMethod("laitier 1", new StateLiquide());
+        ConcretCreatorFruit epice = new ConcretCreatorFruit();
+        Ingredient epice1 = epice.CreateMethod("epice 1", new StateSolide());
+        IngredientInventaire ingredientInventaireFruit = new IngredientInventaire(fruit1 ,134.00);
+        IngredientInventaire ingredientInventaireViande = new IngredientInventaire(viande1 ,132344.60);
+        IngredientInventaire ingredientInventaireLegume = new IngredientInventaire(legume1 ,123434.60);
+        IngredientInventaire ingredientInventaireLaitier = new IngredientInventaire(laitier1 ,13234.60);
+        IngredientInventaire ingredientInventaireEpice = new IngredientInventaire(epice1 ,234552344546.245);
 
         try
         {
            ingredientInventaireEpice.setQuantite(-124312);
+        }
+        catch(IngredientException IE)
+        {
+            System.out.println("not enough Ingredients");
+        }
+
+        try
+        {
+            ingredientInventaireFruit.setQuantite(-124312);
+        }
+        catch(IngredientException IE)
+        {
+            System.out.println("not enough Ingredients");
+        }
+
+        try
+        {
+            ingredientInventaireLaitier.setQuantite(-124312);
+        }
+        catch(IngredientException IE)
+        {
+            System.out.println("not enough Ingredients");
+        }
+
+        try
+        {
+            ingredientInventaireLegume.setQuantite(-124312);
         }
         catch(IngredientException IE)
         {
@@ -61,36 +94,59 @@ public class IngredientTest1 {
 
     @org.junit.Test
 
-    public void test2() throws IngredientException{
+    public void test2() throws IngredientException {
         Boolean good = true;
-        Ingredient epice = Epice.getInstanceEpice("Solide");
-        Ingredient epice2 = Epice.getInstanceEpice("Solide");
-        Ingredient fruit = Fruit.getInstanceFruit("solide");
-        Ingredient fruit2 = Fruit.getInstanceFruit("solide");
-        Ingredient legume = Legume.getInstanceLegume("solide");
-        Ingredient legume2 = Legume.getInstanceLegume("solide");
-        Ingredient laitier = Laitier.getInstanceLaitier("liquide");
-        Ingredient laitier2 = Laitier.getInstanceLaitier("liquide");
+        ConcretCreatorFruit fruit = new ConcretCreatorFruit();
+        Ingredient fruit1 = fruit.CreateMethod("fruit 1", new StateSolide());
+        Ingredient fruit2 = fruit.CreateMethod("fruit 2", new StateSolide());
+        ConcretCreatorViande viande = new ConcretCreatorViande();
+        Ingredient viande1 = viande.CreateMethod("viande 1", new StateSolide());
+        Ingredient viande2 = viande.CreateMethod("viande 2", new StateSolide());
+        ConcretCreatorLegume legume = new ConcretCreatorLegume();
+        Ingredient legume1 = legume.CreateMethod("legume 1", new StateSolide());
+        Ingredient legume2 = legume.CreateMethod("legume 2", new StateSolide());
+        ConcretCreatorLaitier laitier = new ConcretCreatorLaitier();
+        Ingredient laitier1 = laitier.CreateMethod("laitier 1", new StateLiquide());
+        Ingredient laitier2 = laitier.CreateMethod("laitier 2", new StateLiquide());
+        ConcretCreatorFruit epice = new ConcretCreatorFruit();
+        Ingredient epice1 = epice.CreateMethod("epice 1", new StateSolide());
+        Ingredient epice2 = epice.CreateMethod("epice 2", new StateSolide());
 
-        if(epice.hashCode()==epice.hashCode()) {
+/*
+        try
+        {
+            epice1.setStateIngredient( new StateLiquide());
+            epice2.setStateIngredient( new StateSolide());
+        }
+
+        catch (IngredientException ie)
+        {
+           System.out.println("state can't be change");
+        }
+*/
+
+        if(epice1.getState()==epice2.getState() ) {
             good = true;
         }
 
-        if(fruit.hashCode()==fruit2.hashCode()){
+        if(fruit1.getState()==fruit2.getState()){
             good = true;
         }
 
-        if(legume.hashCode()==legume2.hashCode())
+        if(legume1.getState()!=legume2.getState())
         {
             good = true;
         }
 
-        if(laitier.hashCode()==laitier2.hashCode()){
+        if(laitier1.getState()!=laitier2.getState()){
             good = true;
         }
+
+
+
         assertEquals(true,good);
 
     }
 
-     */
+
 }
